71ba0623816e5175facfcd47933d462f45ee6deb
leegeonho <gunho.lee@samsung.com>
2022-11-16 23:18:13 +0900

    add log4jdbc and add mybatis setting

--- mybatis/src/main/java/my/study/mybatis/dao/MecfsirmtDAO.java
+++ mybatis/src/main/java/my/study/mybatis/dao/MecfsirmtDAO.java
@@ -1,5 +1,7 @@
 package my.study.mybatis.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
--- mybatis/src/test/java/my/study/mybatis/dao/MecfsirmtDAOTest.java
+++ mybatis/src/test/java/my/study/mybatis/dao/MecfsirmtDAOTest.java
@@ -1,11 +1,12 @@
 package my.study.mybatis.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import lombok.extern.slf4j.Slf4j;
-import my.study.mybatis.entity.Mecfsirmt;
 
 @SpringBootTest
 @Slf4j
@@ -17,10 +18,12 @@ public class MecfsirmtDAOTest {
 	@Test
 	public void getMecfsirmt() {
 
-		Mecfsirmt mecfsirmt = mecfsirmtDAO.getMecfsirmt(11980L);
+		var cnosRcrdMstrNoP = 11980L;
+		var mecfsirmt = mecfsirmtDAO.getMecfsirmt(cnosRcrdMstrNoP);
 
 		log.info("mecfsirmt={}", mecfsirmt);
 
+		assertEquals(mecfsirmt.getCnosRcrdMstrNo(), cnosRcrdMstrNoP);
 	}
 
 }
